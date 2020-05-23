9a6efab696da224a406d7c9f011382104f1f1321
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-05-23 12:01:58 +0530

    updated

--- pages/opportunities.php
+++ pages/opportunities.php
@@ -13,13 +13,48 @@
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
     <title>Depiction Opportunities</title>
-    <link rel="stylesheet" type="text/css" href="../styles/form.css">
+    <link rel="stylesheet" type="text/css" href="../styles/opportunities.css">
 </head>
 
 <body>
     <?php include "./navbar.php"; ?>
-    <div class="resume-form">
+    <div class="depicting">
         <h1>Depiction Opportunities</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur lobortis nulla eget
+            ullamcorper. Proin sed tortor mattis, feugiat quam sit amet, ultrices leo. Etiam hendrerit at risus nec
+            lobortis. Integer sit amet ante in leo rutrum ornare. Proin lobortis dapibus vulputate. Nullam cursus dolor
+            diam. Ut et felis eu augue sollicitudin ultricies nec ac tortor. Vestibulum ante ipsum primis in faucibus
+            orci luctus et ultrices posuere cubilia curae; Quisque quis ultricies massa. Fusce molestie nulla nunc, a
+            dictum sem maximus nec.</p>
+
+        <p>Cras fringilla varius vulputate. Etiam tincidunt purus nec ligula porta tincidunt. Pellentesque placerat
+            cursus pharetra. Vivamus ac aliquet arcu. Nam in diam augue. Vestibulum vehicula in libero non sagittis. Ut
+            auctor sapien ligula, in semper orci dignissim eget. Praesent in tellus id leo tristique hendrerit vitae sit
+            amet diam. Aliquam ac augue et nulla vulputate malesuada at in dolor. Donec ac justo felis.</p>
+
+        <p>Pellentesque suscipit eu arcu et porttitor. Pellentesque pellentesque nisi id nisl placerat fermentum.
+            Maecenas quis mauris aliquam arcu ultricies convallis ut imperdiet libero. Nullam condimentum tempus urna,
+            non volutpat augue egestas ac. Maecenas suscipit eu lorem ut finibus. Praesent blandit eleifend erat
+            ullamcorper posuere. Pellentesque dignissim eros quis dui eleifend malesuada.</p>
+
+        <p>Sed convallis ultrices feugiat. Duis consectetur erat non dui aliquet, sit amet pulvinar enim dignissim.
+            Mauris blandit sollicitudin orci eget eleifend. Fusce suscipit ante id cursus rutrum. In quis turpis eget
+            quam gravida iaculis. Pellentesque in augue et arcu volutpat viverra sed et urna. Maecenas turpis nisi,
+            faucibus a tempus ac, hendrerit in est. Integer vitae quam scelerisque, malesuada quam vitae, aliquet arcu.
+            Vivamus sagittis nisi eget ligula placerat tristique. Aenean a ante nisl. Nunc dapibus faucibus ligula, nec
+            interdum justo bibendum sed. Nunc sagittis lorem augue, nec rhoncus nisl molestie nec. Sed ultrices
+            venenatis lorem, in finibus dolor interdum sed. Proin eu efficitur eros. Vestibulum ut placerat massa. Nulla
+            aliquet hendrerit porta.</p>
+
+        <p>Pellentesque scelerisque consequat nibh id efficitur. Nunc placerat enim dictum, rhoncus odio id, finibus
+            orci. Integer non augue at nulla ornare molestie. Pellentesque dignissim tristique ipsum, ac auctor massa
+            placerat quis. Cras dignissim, enim vitae vehicula pretium, urna dui mattis ligula, a imperdiet felis urna
+            vel dolor. Suspendisse ac ipsum ex. Phasellus mi eros, egestas vitae leo nec, feugiat sagittis metus.
+            Maecenas vitae urna feugiat, lobortis urna eget, lacinia leo. Vivamus dui nisi, egestas quis fermentum ut,
+            aliquet et risus. Nunc rutrum, tortor in hendrerit accumsan, ligula elit auctor leo, at vulputate neque
+            sapien quis lectus. Etiam sit amet nisi in nisi viverra lobortis. Aenean odio dui, tristique id luctus ac,
+            ullamcorper quis nisi. Donec iaculis luctus ex, vitae ultricies dolor. Proin euismod sagittis nunc, in
+            consequat neque hendrerit tempus. Cras eu massa quis ante rhoncus dictum.</p>
     </div>
 
     <!-- Optional JavaScript -->
