754fb6813da99799829c47181113fa29245cdcb0
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-05-24 10:35:50 +0530

    updated

--- js/form.js
+++ js/form.js
@@ -1,1 +1,38 @@
-document.querySelectorAll("nav ul li a")[1].style.color="#c7c4c3";
+$(document).ready(function () {
+    document.querySelectorAll("nav ul li a")[1].style.color = "#c7c4c3";
+
+    //--------------------------------Submitting form--------------------------------------//
+    
+    $("#submit").click(function () {
+        var info1 = document.getElementById("info1").value;
+        var info2 = document.getElementById("info2").value;
+        var info3 = document.getElementById("info3").value;
+        var info4 = document.getElementById("info4").value;
+        var info5 = document.getElementById("info5").value;
+        var info6 = document.getElementById("info6").value;
+        var submit = document.getElementById("submit").value;
+        var info = {
+            info1: info1,
+            info2: info2,
+            info3: info3,
+            info4: info4,
+            info5: info5,
+            info6: info6,
+            submit: submit
+        };
+        $.post('./pages/form.php', info, function (data, status) {
+            console.log(JSON.parse(data));
+            const err = JSON.parse(data)["err"];
+            const stat = JSON.parse(data)["status"];
+            console.log(err, stat);
+            if (err != "") {
+                document.getElementById("err").innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+            ${err}
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>`;
+            }
+        });
+    });
+});
---
+++ js/resume.js
@@ -0,0 +1,31 @@
+function getUrlVars() {
+    var vars = [],
+        hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+$(document).ready(function () {
+
+    //-------------------------------search intern by id-------------------------------------//
+    
+    var id = getUrlVars()['id'];
+    $.get(`./pages/resume.php?id=${id}`, function (data, status) {
+        console.log(JSON.parse(data));
+        if (JSON.parse(data)['status'] != "")
+            window.location.replace("./admin.html");
+        else {
+            var intern = JSON.parse(data)['intern'];
+            document.getElementById("intern").innerHTML += `info1 : ${intern['info1']}<br>`;
+            document.getElementById("intern").innerHTML += `info2 : ${intern['info2']}<br>`;
+            document.getElementById("intern").innerHTML += `info3 : ${intern['info3']}<br>`;
+            document.getElementById("intern").innerHTML += `info4 : ${intern['info4']}<br>`;
+            document.getElementById("intern").innerHTML += `info5 : ${intern['info5']}<br>`;
+            document.getElementById("intern").innerHTML += `info6 : ${intern['info6']}`;
+        }
+    });
+});
